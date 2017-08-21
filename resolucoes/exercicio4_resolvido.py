d58b26064f6b39e1a9d577012381786491f9ed18
PauloSalvatore <paulo.salvatore@hotmail.com.br>
2017-08-21 15:07:42 -0300

    Exercício 4 adicionado

---
+++ resolucoes/exercicio4_resolvido.py
@@ -0,0 +1,19 @@
+﻿"""
+Exercício 4
+
+Nome: Par ou Ímpar
+Objetivo: Receber um número do usuário e informar se ele é par ou ímpar.
+
+1 - Crie um programa que receba do usuário um número;
+2 - Caso o número seja par, exibir a mensagem no console "O número {numero} é par.";
+3 - Caso contrário, exibir a mensagem "O número {numero} é ímpar.".
+"""
+
+# Resolução
+
+numero = int(input("Insira um número: "))
+
+if numero % 2 == 0:
+	print("O número {} é par.".format(numero))
+else:
+	print("O número {} é ímpar.".format(numero))
