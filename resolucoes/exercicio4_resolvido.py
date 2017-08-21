ca54ee153ce155e34df83dc75a1d6fda8058badc
PauloSalvatore <paulo.salvatore@hotmail.com.br>
2017-08-21 15:58:35 -0300

    Dificuldade do Exercício 4 adicionada

--- resolucoes/exercicio4_resolvido.py
+++ resolucoes/exercicio4_resolvido.py
@@ -3,6 +3,7 @@ Exercício 4
 
 Nome: Par ou Ímpar
 Objetivo: Receber um número do usuário e informar se ele é par ou ímpar.
+Dificuldade: Principiante
 
 1 - Crie um programa que receba do usuário um número;
 2 - Caso o número seja par, exibir a mensagem no console "O número {numero} é par.";
