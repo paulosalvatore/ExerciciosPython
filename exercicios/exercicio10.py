ba9d61da3236e943f415d5fa9dc31b9d72ce920a
PauloSalvatore <paulo.salvatore@hotmail.com.br>
2017-12-09 17:18:49 -0200

    Exercício 10 adicionado

---
+++ exercicios/exercicio10.py
@@ -0,0 +1,11 @@
+﻿"""
+Exercício 10
+
+Nome: A de Amor, B de Baixinho
+Objetivo: Receber o nome, extrair a primeira letra e fazer comparações.
+
+1 - Escreva um programa que receba o nome de uma pessoa;
+2 - Extraia a primeira letra do nome;
+3 - Compare a primeira letra de modo que, caso seja "A", exiba na tela "A de Amor", caso seja "B", exiba na tela "B de Baixinho", caso contrário, exiba na tela "Seu nome não é legal.";
+4 - Faça com que o programa funcione indepente se for digitado um nome com letras minúsculas ou maiúsculas.
+"""
