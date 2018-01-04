e6bd08f187ae4e5f4fff1e24c9dc74e0e7f5ec0a
PauloSalvatore <paulo.salvatore@hotmail.com.br>
2018-01-04 14:55:49 -0200

    Exercício 25 adicionado
    Resolução será postada em breve

---
+++ exercicios/exercicio25.py
@@ -0,0 +1,20 @@
+﻿"""
+Exercício 25
+
+Nome: Cadastro de Pessoas
+Objetivo: Criar uma aplicação que cadastre, liste, consulte e remova pessoas utilizando um dicionário.
+Dificuldade: Avançado
+
+1 - Crie uma aplicação que rode infinitamente.
+2 - A aplicação deverá perguntar ao usuário se deseja realizar uma das seguintes ações (independente se digitar maiúsculo ou minúsculo):
+'C' ou 'Cadastrar' irá cadastrar uma nova pessoa ao dicionário;
+'L' ou 'Listar' irá listar as pessoas cadastradas no dicionário;
+'R' ou 'Remover' irá remover uma pessoa (buscando pelo nome);
+'E' ou 'Editar' irá editar uma pessoa previamente cadastrada;
+3 - A pessoa terá os seguintes dados: nome, sobrenome, telefone e CPF.
+4 - A cada ação realizada, o sistema deverá informar uma mensagem de sucesso.
+5 - Em caso de ações que procurem pessoas já cadastradas no sistema (editar ou remover), caso a pessoa não seja encontrada, exiba essa mensagem para o usuário.
+6 - Você deverá declarar uma função que recebe um nome de pessoa digitado pelo usuário e procure no dicionário por aquela pessoa. Caso encontre, retorne a pessoa encontrada. Caso contrário, exiba uma mensagem de que a pessoa não foi encontrada e pergunte novamente pelo nome da pessoa.
+7 - A qualquer momento da aplicação, o usuário poderá cadastrar, listar, remover ou editar pessoas.
+8 - Caso ele acesse alguma das opções acima e não queira prosseguir, digitar a palavra 'cancelar' irá retornar ao menu principal.
+"""
