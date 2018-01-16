c355f13652d50a786dbe0134153fa4dfaa8f82ec
PauloSalvatore <paulo.salvatore@hotmail.com.br>
2018-01-16 11:52:13 -0200

    Exercício 27 adicionado

---
+++ exercicios/exercicio27.py
@@ -0,0 +1,25 @@
+"""
+Exercício 27
+
+Nome: Urna Eletrônica
+Objetivo: Desenvolver um sistema de urna eletrônica que funcione apenas para a eleição de prefeito de uma cidade.
+Dificuldade: Avançado
+
+1 - Criar um dicionário com informações básicas dos candidatos à prefeitura.
+2 - A aplicação deverá funcionar exatamente como uma urna eletrônica, contabilizando
+votos válidos, brancos e nulos.
+3 - Exiba para o usuário: "Informe seu voto para a prefeitura de São Paulo (2 dígitos): "
+4 - A pessoa também pode digitar 'branco' em vez de um número.
+5 - Assim que o usuário digitar um número, verifique se é um político existente e se
+o político possui aquele cargo específico.
+6 - Caso possua, informe o nome do político e o partido e peça para o usuário informar
+uma ação: confirmar, corrigir ou cancelar.
+- Confirmar computa o voto.
+- Corrigir pergunta o voto novamente.
+- Cancelar pergunta o voto novamente.
+7 - Coloque um comando 'encerrar votação' que finaliza a votação e exibe as informações abaixo:
+- Informar a quantidade de votos válidos, brancos e nulos;
+- Checar se o político mais votado possui mais de 50% dos votos ou se haverá segundo turno.
+- Caso tenha vencedor: Informar quem ganhou e qual a porcentagem de votos que ele teve;
+- Exibir na tela um político por linha, com a quantidade de votos recebidos e a porcentagem equivalente.
+"""
