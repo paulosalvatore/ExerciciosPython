477c264d0a4f53480d6bc4917e95366b044c80ea
PauloSalvatore <paulo.salvatore@hotmail.com.br>
2018-02-07 19:48:42 -0200

    Exercício 28 adicionado

---
+++ resolucoes/exercicio28_resolvido.py
@@ -0,0 +1,12 @@
+"""
+Exercício 28
+
+Nome: Planilhas de Políticos
+Objetivo: Desenvolver uma aplicação que busque aleatoriamente alguns políticos e salve determinadas informações em uma planilha de Excel.
+Dificuldade: Avançado
+
+1 - Pegar um político na API http://politicos.olhoneles.org/api/v0/politicians/1/
+2 - Armazenar todas as informações desse político em uma planilha de Excel seguindo esse modelo: http://gg.gg/excel_politicos
+3 - Depois de pronto, faça o código pegar 10 números de ID aleatoriamente e gravar uma worksheet para cada político.
+4 - O nome de cada página deve ser o nome do político e as páginas deverão estar ordenadas por ordem alfabética.
+"""
